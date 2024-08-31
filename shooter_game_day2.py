075b1427ac0a88b3a0d41dfa2091c2431dbaf9a7
RoeunApireak <apireak.roeun.datau@gmail.com>
2024-08-31 09:57:53 +0700

    commit change day2

--- shooter_game_day2.py
+++ shooter_game_day2.py
@@ -4,47 +4,52 @@ from pygame import *
 
 from spriteClass import Player, GameSprite
 
-# chill class of GameSprite
+from random import randint
+
+# child class of GameSprite
 class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
 
-        global lost
-        if self.rect.y > 500:
-            self.rect.x = randint(80, 420)
-            self.rect.y = 0
+        global missing
 
-            lost += 1
+        if self.rect.y > 500:
+            self.rect.x = randint(5, 620)
+            self.rect.y = 0 
+
+            missing += 1
+
 
 window = display.set_mode((700,500))
 display.set_caption('Shooter Game')
 
 background = transform.scale(image.load('galaxy.jpg'), (700, 500))
 
-# clock = time.Clock()
+clock = time.Clock()
 
 mixer.init()
 mixer.music.load('space.ogg')
 # mixer.music.play()
 
-rocket = Player('rocket.png', 5 , 420 , 3)
+rocket = Player('rocket.png', 5 , 420 , 5)
 
-from random import randint
-monsters = sprite.Group()
-for i in range(1, 6):
-    monster = Enemy(player_image='ufo.png', 
-                    player_x=randint(80, 420), 
-                    player_y=-40, 
-                    player_speed=5 )
-    monsters.add(monster)
+ufo_group = sprite.Group()
+for i in range(1, 11):
+    ufo = Enemy(player_image='ufo.png', 
+                player_x=randint(40, 620), 
+                player_y= 5, 
+                player_speed= randint(1, 3) )
+    ufo_group.add(ufo)
 
-font.init()
-style = font.Font(None, 36)
 
 run = False
-lost = 0
 
 finish = False
+
+font.init()
+style = font.Font(None, 30)
+
+missing = 0
 
 while not run:
 
@@ -52,23 +57,23 @@ while not run:
         if e.type == QUIT:
             run = True
 
-        
     if not finish:
         window.blit(background, (0,0))
 
-        text_lose = style.render("Missed: " + str(lost), 1, (255, 255, 255))
-        window.blit(text_lose, (10, 20))
+        missed = style.render('Missed: ' + str(missing), 1, (255, 255, 255))
+        window.blit(missed, (5,5))
+
+        score = style.render('Scores: ', 1, (255, 255, 255))
+        window.blit(score, (5,30))
+
 
         rocket.reset(window)
         rocket.controller()
 
-        monsters.update()
+        ufo_group.update()
 
-        monsters.draw(window)
+        ufo_group.draw(window)
 
         display.update()
 
-
-    time.delay(50)
-    # display.update()
-
+        time.delay(50) 
